e9ebf72422066287376fd8dbaee35bdb45d296c7
Serena <serena.kei@autodesk.com>
2023-06-10 01:01:18 -0700

    clean up unused folders and files, update readme (#3)
    Co-authored-by: Serena Kei <serena.kei@grafana.com>

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "rc-drawer/assets/index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./theme";
 
 const root = ReactDOM.createRoot(
@@ -15,8 +14,3 @@ root.render(
     <App />
   </ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
